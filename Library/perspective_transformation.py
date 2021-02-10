245586c3bb7ce46bf1b5a20b7423cbaad4a560f7
Javier Manobanda <51manobandajavier@gmail.com>
2021-02-10 14:06:00 -0500

    Se genera clase para cambio de perspectiva se toma 4 puntos de la imagen original y se crea la imagen cambiada la perspectiva

---
+++ Library/perspective_transformation.py
@@ -0,0 +1,38 @@
+import cv2 as cv
+import numpy as np
+
+
+class Perspective:
+
+    def __init__(self, imagen, points):
+        self.imagen = imagen
+        self.points = points
+
+    def perspectiveShow(self):
+
+        # Se coloca primero los puntos de la imagen a mover
+        """ Considera el orden de los puntos
+        punto1   punto2
+            -------
+            |       |
+            |       |
+            -------
+        punto3   punto4
+        """
+        height, width, channel = self.imagen.shape
+
+        # Aqui se hace un acorrección de la logica debido a que cambia la ubicación de los puntos
+        firstPoints = np.float32(
+            [self.points[0], self.points[1], self.points[3], self.points[2]])
+
+        secondsPoint = np.float32(
+            [[0, 0],
+             [width, 0],
+             [0, height],
+             [width, height]])
+
+        matrixM = cv.getPerspectiveTransform(firstPoints, secondsPoint)
+
+        dts = cv.warpPerspective(self.imagen, matrixM, (width, height))
+
+        cv.imshow('Perspectiva', dts)
--- Library/read_image.py
+++ Library/read_image.py
@@ -1,7 +1,8 @@
 import cv2 as cv
 import numpy as np
+from perspective_transformation import Perspective
 
-path_image = '../imagen_examples/corazon.jpg'
+path_image = '../imagen_examples/cat.jpg'
 origin_name = 'Origin'
 color_point = (0, 255, 0)
 color_line = (0, 0, 255)
@@ -9,7 +10,7 @@ points = []
 
 
 def select_points(event, x, y, flags, param):
-    """Function to draw points to cut the imagen 
+    """Function to draw points to cut the imagen
 
     Args:
         event (event): event OpenCv
@@ -54,12 +55,22 @@ def join_points(point_1, point_2):
 
 def reset_points():
     points.clear()
-    imagen = cv.imread(path_image, cv.IMREAD_COLOR)
+    update_screen()
 
 
 if __name__ == '__main__':
+
     imagen = cv.imread(path_image, cv.IMREAD_COLOR)
 
+    """scalePercent = 0
+    width = int(imagen.shape[1]*scalePercent/100)
+    height = int(imagen.shape[0]*scalePercent/100)
+    resized = cv.resize(imagen, (width, height), interpolation=cv.INTER_AREA)
+    imagen = resized"""
+
+    copyImagen = imagen.copy()  # have a copy of te imagen
+
+    print(imagen.shape)
     update_screen()
     cv.setMouseCallback(origin_name, select_points)
 
@@ -72,6 +83,10 @@ if __name__ == '__main__':
         if key == 27:  # Press ESC to finish
             break
         elif key == 114:  # Press r to reset
+            imagen = copyImagen.copy()
             reset_points()
+        elif key == 112:  # Press p to change perspective
+            changePerspective = Perspective(copyImagen, points)
+            changePerspective.perspectiveShow()
 
     cv.destroyAllWindows()
