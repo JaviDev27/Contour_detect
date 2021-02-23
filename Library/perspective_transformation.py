861faeb77169bf4bdcc2c398cd2f535d3b564828
Javier Manobanda <51manobandajavier@gmail.com>
2021-02-23 17:16:39 -0500

    Se cambia la clase perspectiva para que detecte la hoja en blnaco y que automáticamente haga la perspectiva. se usa la relacion de hoja y pixel para tener una medida ma aproximada del contorno

--- Library/perspective_transformation.py
+++ Library/perspective_transformation.py
@@ -4,9 +4,11 @@ import numpy as np
 
 class Perspective:
 
-    def __init__(self, imagen, points):
-        self.imagen = imagen
-        self.points = points
+    def __init__(self, image, width, height):
+        self.image = image
+        self.width = width
+        self.height = height
+        self.firstPoints = None
 
     def perspectiveShow(self):
 
@@ -19,23 +21,52 @@ class Perspective:
             -------
         punto3   punto4
         """
-        # Se toma el tamaño de la imagen original
-        height, width, channel = self.imagen.shape
-
-        # Aqui se hace un acorrección de la logica debido a que cambia la ubicación de los puntos
-        firstPoints = np.float32(
-            [self.points[0], self.points[1], self.points[3], self.points[2]])
 
         secondsPoint = np.float32(
             [[0, 0],
-             [width, 0],
-             [0, height],
-             [width, height]])
+             [self.width, 0],
+             [0, self.height],
+             [self.width, self.height]])
 
-        matrixM = cv.getPerspectiveTransform(firstPoints, secondsPoint)
+        matrixM = cv.getPerspectiveTransform(self.firstPoints, secondsPoint)
 
-        dts = cv.warpPerspective(self.imagen, matrixM, (width, height))
-
-        cv.imshow('Perspectiva', dts)
-
+        dts = cv.warpPerspective(self.image, matrixM,
+                                 (self.width, self.height))
         return dts
+
+    def __order_points(self, points):
+        n_points = np.concatenate(
+            [points[0], points[1], points[2], points[3]]).tolist()
+
+        y_order = sorted(n_points, key=lambda n_points: n_points[1])
+
+        x1_order = y_order[:2]
+        x1_order = sorted(x1_order, key=lambda x1_order: x1_order[0])
+
+        x2_order = y_order[2:4]
+        x2_order = sorted(x2_order, key=lambda x2_order: x2_order[0])
+
+        return [x1_order[0], x1_order[1], x2_order[0], x2_order[1]]
+
+    def roi_imagen(self):
+
+        perspectiva = None
+
+        image_copy = self.image.copy()
+
+        gray = cv.cvtColor(image_copy, cv.COLOR_BGR2GRAY)
+        _, th = cv.threshold(gray, 80, 255, 0)
+
+        contours = cv.findContours(
+            th, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[0]
+        contours = sorted(contours, key=cv.contourArea, reverse=True)[:1]
+
+        for c in contours:
+            epsilon = 0.01*cv.arcLength(c, True)
+            approx = cv.approxPolyDP(c, epsilon, True)
+            if len(approx) == 4:
+                points = self.__order_points(approx)
+                self.firstPoints = np.float32(points)
+                perspectiva = self.perspectiveShow()
+
+        return perspectiva
